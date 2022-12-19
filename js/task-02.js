db08c7c63cddbc881e3e992b81ddeedc16991106
Vitalii Chaban <vitaliichaban2022@gmail.com>
2022-12-19 16:12:43 -0500

    made task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,14 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
-];
+]; for (const ingredient of ingredients) {
+
+  const list = document.createElement('li');
+  list.classList.add('item');
+  list.textContent = ingredient;
+ 
+  const lists = document.querySelector('ul');
+  lists.prepend(list);
+
+  console.log(list);
+};
