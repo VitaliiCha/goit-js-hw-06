d04a216635542f9be64bc6eddf770a45bdbe1a92
Vitalii Chaban <vitaliichaban2022@gmail.com>
2022-12-26 17:51:31 -0500

    made task-(08; 09).js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,34 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit",  (event)=> {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+      if (email.value.trim === '' || password.value === '') {
+        return alert('All fields must be filled!')
+      }
+      const userForm = {
+          email: email.value, password: password.value
+      }
+      console.log(userForm);
+      event.currentTarget.reset()
+});
+
+// const form = document.querySelector(".login-form");
+
+// form.addEventListener("submit", handleSubmit);  
+//   function handleSubmit (event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password }
+//   } = event.currentTarget;
+//       if (email.value.trim === '' || password.value === '') {
+//         return alert('All fields must be filled!')
+//       }
+//       const userForm = {
+//           email: email.value, password: password.value
+//       }
+//       console.log(userForm);
+//       event.currentTarget.reset()
+// };
