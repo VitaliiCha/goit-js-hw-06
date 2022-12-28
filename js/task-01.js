febb36552aa8b0549685382645f0ae07ff4f13ff
Vitalii Chaban <vitaliichaban2022@gmail.com>
2022-12-27 23:06:35 -0500

    fix task(01; 03;).js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,8 @@
-const NumCategories = document.querySelectorAll(".item");
+let NumCategories = document.querySelectorAll(".item");
 console.log("Number of categories:", NumCategories.length);
 
-
-console.log("Category:", NumCategories[0].firstElementChild.firstChild.textContent);
-console.log('Elements:', NumCategories[0].lastChild.previousElementSibling.children.length)
-
-console.log("Category:", NumCategories[1].firstElementChild.firstChild.textContent);
-console.log('Elements:', NumCategories[1].lastChild.previousElementSibling.children.length)
-
-console.log("Category:", NumCategories[2].firstElementChild.firstChild.textContent);
-console.log('Elements:', NumCategories[2].lastChild.previousElementSibling.children.length);
+for (let i = 0; i < NumCategories.length; i++) {
+    const element = NumCategories[i];
+    console.log("Category:", NumCategories[i].firstElementChild.firstChild.textContent);
+    console.log('Elements:', NumCategories[i].lastChild.previousElementSibling.children.length);
+};
